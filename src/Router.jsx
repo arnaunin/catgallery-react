65ec06f8635dc0259f797ace85914a20dc2353f3
Arnau Nin <ninbsnss92@gmail.com>
2026-01-19 20:56:20 +0100

    Some copilot adjustments

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Favoritos from './pages/Favoritos.jsx'
 
 const Router = () => {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/favoritos' element={<Favoritos />}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
